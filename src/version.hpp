39af707a662912eba901c3152ba8dd06293a2165
Tino Didriksen <mail@tinodidriksen.com>
2015-06-10 17:45:28 +0000

    Add package info files
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk/apertium-simpleton@60708 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ src/version.hpp
@@ -0,0 +1,33 @@
+/*
+* Copyright (C) 2015, Tino Didriksen <mail@tinodidriksen.com>
+*
+* This file is part of apertium-simpleton
+*
+* apertium-simpleton is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* apertium-simpleton is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with apertium-simpleton.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#ifndef VERSION_HPP_fc74fd919e58451d89f5d0bba814925a
+#define VERSION_HPP_fc74fd919e58451d89f5d0bba814925a
+
+#include <stdint.h>
+
+const char* const COPYRIGHT_STRING = "Copyright (C) 2015 Tino Didriksen. Licensed under GPLv3+";
+
+const uint32_t APP_VERSION_MAJOR = 0;
+const uint32_t APP_VERSION_MINOR = 1;
+const uint32_t APP_VERSION_PATCH = 0;
+const uint32_t APP_REVISION = 60703;
+
+#endif
